e7bd6ac97574dfaf05378944442812eea9d5e063
Motionlessx <80837189+Motionlessx@users.noreply.github.com>
2022-07-03 16:12:30 +0300

    bv

--- auto/src/main/java/Main.java
+++ auto/src/main/java/Main.java
@@ -1,16 +1,4 @@
 public class Main {
-    public static void main(String[] args) {
-        Hello("user");
-    }
-    public static void Hello(String somebody) {
-        System.out.println("Hello " + somebody);
-    }
-    public Square s = new Square(5) {
-
-    };
-    public static double area(Square s){
-        return s.x * s.x;
-    }
 
 }
 
